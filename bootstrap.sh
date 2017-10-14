cd03b76a754a2d8aff63fffe3e2f3500583e98bf
Mustafa Erkan Başar <erkan@mebasar.com>
2017-10-14 03:29:16 +0200

    made it to see folder symlinks as files (-n option)

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,23 +3,23 @@
 BASEDIR="$(pwd)"
 
 # Git
-ln -sf $BASEDIR/gitconfig $HOME/.gitconfig
+ln -sfn $BASEDIR/gitconfig $HOME/.gitconfig
 
 # Shell
-ln -sf $BASEDIR/shell/bashrc $HOME/.bashrc
-ln -sf $BASEDIR/shell/bash_profile $HOME/.bash_profile
-ln -sf $BASEDIR/shell/bash_aliases $HOME/.bash_aliases
-ln -sf $BASEDIR/shell/bash_logout $HOME/.bash_logout
+ln -sfn $BASEDIR/shell/bashrc $HOME/.bashrc
+ln -sfn $BASEDIR/shell/bash_profile $HOME/.bash_profile
+ln -sfn $BASEDIR/shell/bash_aliases $HOME/.bash_aliases
+ln -sfn $BASEDIR/shell/bash_logout $HOME/.bash_logout
 
 # Custom Scripts
-sudo ln -sf $BASEDIR/scripts/tunnel.sh /usr/bin/tunnel
-sudo ln -sf $BASEDIR/scripts/srunl.sh /usr/bin/srunl
+sudo ln -sfn $BASEDIR/scripts/tunnel.sh /usr/bin/tunnel
+sudo ln -sfn $BASEDIR/scripts/srunl.sh /usr/bin/srunl
 
 # Guake
-ln -sf $BASEDIR/guake/ $HOME/.gconf/apps/guake
+ln -sfn $BASEDIR/guake/ $HOME/.gconf/apps/guake
 
 # Atom
-ln -sf $BASEDIR/atom/ $HOME/.atom
+ln -sfn $BASEDIR/atom/ $HOME/.atom
 
 # Autostart
-ln -sf $BASEDIR/autostart/ $HOME/.config/autostart
+ln -sfn $BASEDIR/autostart/ $HOME/.config/autostart
