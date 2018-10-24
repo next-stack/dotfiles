d545254377468d0eeb6c2dd2d7bfd6d6d6c7eeff
M. Erkan Basar <erkan@mebasar.com>
2018-10-24 14:23:35 +0200

    Removed lamachine traces from shell scripts

--- shell/nvidia-fix.sh
+++ shell/nvidia-fix.sh
@@ -1,6 +1,6 @@
 
 # This is not a script to run, but to remember a solution to problem with linux kernel and ndivia driver.
-# This solution works to run nvidia-390 with kernel 4.10-generic
+# This solution works to run nvidia-390 with kernel 4.15-generic
 sudo prime-select nvidia
 sudo update-alternatives --config x86_64-linux-gnu_gl_conf # set to auto mode
 sudo prime-select nvidia
